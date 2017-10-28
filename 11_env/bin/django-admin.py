66a3a5217478096c00c094c4610f6fe157a47685
ruby777 <patrickpowers848@gmail.com>
2017-10-28 15:17:22 -0400

    space invaders game

---
+++ 11_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ruby777/Documents/Python/python_book/alien_invasion/11_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
